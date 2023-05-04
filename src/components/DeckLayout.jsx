f1194f3c57f17530fd732c02470194e4551b563f
memorywanderer <denis.lukin.98@pm.me>
2023-05-04 12:51:52 +0600

    DeckLayout component fix

--- src/components/DeckLayout.jsx
+++ src/components/DeckLayout.jsx
@@ -1,34 +1,38 @@
-import { Outlet, useNavigate, useOutletContext, useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { getCollections } from "../features/collection/collectionSlice";
-import { Spinner } from "./Spinner";
-import { toast } from "react-toastify";
-
-
 export const DeckLayout = () => {
   const { id } = useParams();
-
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const { user } = useSelector(state => state.auth)
-  const { collections, isError, isLoading, message } = useSelector(state => state.collection)
-  const deck = collections.find(c => c._id === id);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user } = useSelector(state => state.auth);
+  const { isError, isLoading, message } = useSelector(state => state.collection);
 
   useEffect(() => {
     if (isError) {
-      toast.error(message)
+      toast.error(message);
     }
     if (!user) {
-      navigate('/login')
+      navigate('/login');
     } else {
-      dispatch(getCollections())
+      dispatch(getCollections());
     }
-  }, [dispatch, navigate, isError, user])
+  }, [dispatch, navigate, isError, user]);
 
-  if (isLoading || !collections.length) {
-    return <Spinner />
+  if (isLoading) {
+    return <Spinner />;
   }
+
+  return (
+    <Deck id={id} />
+  );
+};
+
+const Deck = ({ id }) => {
+  const { collections } = useSelector(state => state.collection);
+  const deck = collections.find(c => c._id === id);
+
+  if (!deck) {
+    return <div>Deck not found</div>;
+  }
+
   return (
     <>
       <Outlet context={deck} />
@@ -37,5 +41,5 @@ export const DeckLayout = () => {
 };
 
 export const useDeck = () => {
-  return useOutletContext()
-}
+  return useOutletContext();
+};
