a7cea32a468e98c38490a293ba890bc9b63a2bb9
memorywanderer <denis.lukin.98@pm.me>
2023-06-20 18:11:21 +0600

    api endpoint fixes

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URL = 'https://myflashcards-api.up.railway.app/api/cards/api/users/'
+const API_URL = 'https://myflashcards-api.up.railway.app/api/users/'
 
 const register = async (userData) => {
   const response = await axios.post(API_URL + 'register', userData)
